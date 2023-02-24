6aac7db5b06485c95be90857a8feda5bef876f77
mmnldm <mremmanueledem>
2023-02-24 23:53:11 +0100

    fixed header color and added marquee

--- layout/header.php
+++ layout/header.php
@@ -5,21 +5,22 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>DNMGD</title>
-<link rel="stylesheet" href="./css/style.css" />
-<link rel="stylesheet" href="./css/strike.css" />
-
+<link rel="stylesheet" href="./css/style.css">
 <link href="https://fonts.cdnfonts.com/css/vcr-osd-mono" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro-v6@44659d9/css/all.css" rel="stylesheet" type="text/css" />
 <script src="https://kit.fontawesome.com/1f649878fb.js" crossorigin="anonymous"></script>
 </head>
-  <body class="font-vcr-osd dark:bg-black dark:text-white">
+  <body class="font-vcr-osd bg-slate-300 dark:bg-black dark:text-white">
+  <marquee height='40' class="border-b border-white dark:bg-slate-300">
+    This is a sample text ...
+</marquee>
     <!---NAVBAR -->
     <nav
-      class="mx-auto max-w-screen-xl border-b border-black dark:border-white lg:px-auto"
+      class="mx-auto max-w-screen-xl dark:border-white lg:px-auto"
     >
       <div class="flex justify-between py-1 px-2 text-lg lg:text-2xl">
         <div>
-          <a class="ml-2 strike" href="index.php">DNMGD </a>
+          <a class="ml-2 line-through " href="index.php">DNMGD </a>
         </div>
 
         <div class="nocart flex">
